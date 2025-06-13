f7ce521be53be4f764a905d716c8035d199e9703
fujulia <juliafu713@gmail.com>
2025-06-13 16:52:05 -0300

    feat: chamada render

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 
 
 // axios.defaults.baseURL = "https://hackathonbackend-f8r2.onrender.com/api";
-axios.defaults.baseURL = 'http://0.0.0.0:19003/api/';
+// axios.defaults.baseURL = 'http://0.0.0.0:19003/api/';
+axios.defaults.baseURL = 'https://santadallah-backend.onrender.com';
+
 
 
 export default axios;
