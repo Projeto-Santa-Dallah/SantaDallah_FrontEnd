a713010ea704a51bc31a0c87950eaeba7865c75e
fujulia <juliafu713@gmail.com>
2025-09-02 09:08:29 -0300

    feat: em  progresso

--- src/views/administrador/PerfilView.vue
+++ src/views/administrador/PerfilView.vue
@@ -1,3 +1,21 @@
 <template>
-    perfil adm
+    <section>
+        <img src="/src/assets/imagens/emProgresso.jpg" alt="">
+    </section>
 </template>
+
+<style scoped>
+
+section{
+    width: 100%;
+    height: 80vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+section img{
+    width: 30%;
+
+}
+
+</style>
