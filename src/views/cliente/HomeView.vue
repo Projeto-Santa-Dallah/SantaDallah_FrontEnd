58b87d3169a025bdd7f1cbb8f0e45d40207211d6
isabellirosa <isabelli.ifc@gmail.com>
2025-09-06 19:21:08 -0300

    feat: Adicionando animação na home do cliente #14

--- src/views/cliente/HomeView.vue
+++ src/views/cliente/HomeView.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="container-home">
-    <div class="fundo"><img src="@/assets/imagens/fundo.png" /></div>
+    <!-- Fundo com GIF -->
+    <div class="fundo"></div>
+
+    <!-- Conteúdo -->
     <div class="itens">
       <div class="titulos">
-        <h1>Trazendo mais doçura e <br/> amor para o seu <br/> momento especial.</h1>
+        <h1>
+          Trazendo mais doçura e <br />
+          amor para o seu <br />
+          momento especial.
+        </h1>
         <h2>
-          Sempre trabalhando e pensando com muito amor e <br/> dedicação para levar o melhor
-          para vocês.
+          Sempre trabalhando e pensando com muito amor e <br />
+          dedicação para levar o melhor para vocês.
         </h2>
         <button>
           Saiba mais sobre os doces
@@ -27,59 +34,74 @@
           </svg>
         </button>
       </div>
-      <img class="bolo" src="@/assets/imagens/boloHome.png" />
     </div>
   </div>
 </template>
+
 <style scoped>
 .container-home {
   width: 100%;
   height: 100%;
+  position: relative;
+  overflow: hidden;
 }
+/* GIF de fundo */
+.fundo {
+  position: fixed; /* fica preso na tela */
+  top: 0;
+  left: 0;
+  width:90%;
+  height: 115vh; /* ocupa a altura da tela */
+  margin-top: -10vh;
+  margin-left: 163px;
+  background: url("@/assets/imagens/home.gif") no-repeat center center;
+  background-size: cover;
+  z-index: -1; /* fica atrás do conteúdo */
+}
+
+
+/* Conteúdo */
+.itens {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 99px;
+  position: relative;
+  z-index: 1; /* fica acima do fundo */
+}
+
 .bolo {
   height: 70vh;
 }
-.itens {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  padding: 0px 99px;
+.titulos{
+  margin-top: 170px;
 }
-h1{
-    margin-top: 0;
-    color:#191645;
-    font-size: 40px;
+h1 {
+  margin-top: 0;
+  color: #191645;
+  font-size: 48px;
 }
-h2{
-    font-size:16px ;
+
+h2 {
+  font-size: 18px;
 }
-button{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-weight:550;
-    margin-top: 20px;
-    border:1px solid #191645;
-    background-color: #191645;
-    color: white;
-    border-radius: 50px;
-    font-size: 16px;
-    padding: 15px;
-}
-button:hover{
-    cursor: pointer;
-    background-color:rgb(9, 7, 36);
-}
-.fundo {
-  z-index: -33;
+
+button {
   display: flex;
-  justify-content: end;
-  top: 0;
-  position: absolute;
-  width: 100vw;
-  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-weight: 550;
+  margin-top: 20px;
+  border: 1px solid #191645;
+  background-color: #191645;
+  color: white;
+  border-radius: 50px;
+  font-size: 18px;
+  padding: 15px;
 }
-.fundo img {
-  height: 100%;
+
+button:hover {
+  cursor: pointer;
+  background-color: rgb(9, 7, 36);
 }
 </style>
