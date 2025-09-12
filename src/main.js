2015e23365fa2db02e158e4aad8510af25f43d57
isabellirosa <isabelli.ifc@gmail.com>
2025-09-12 16:04:23 -0300

    Feat: desing home do admin #14

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 import './assets/main.css'
-
+import 'v-calendar/style.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -8,12 +8,19 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar';
 import "./plugins/axios"
 const app = createApp(App)
 
 
 app.use(createPinia())
 app.use(router)
+// Use plugin defaults (optional)
+app.use(setupCalendar, {})
+
+// Use the components
+app.component('VCalendar', Calendar)
+app.component('VDatePicker', DatePicker)
 
 
 app.mount('#app')
--- src/views/administrador/HomeView.vue
+++ src/views/administrador/HomeView.vue
@@ -1,12 +1,23 @@
 <script setup>
 import OrcamentoProduto from '@/components/administrador/grafico/OrcamentoProduto.vue'
+import SaboresFavoritos from '@/components/administrador/grafico/SaboresFavoritos.vue'
+import { ref } from 'vue';
+
+const selectedColor = ref('purple');
+const attrs = ref([
+  {
+    key: 'test',
+    highlight: true,
+    dates: { start: new Date(2019, 3, 15), end: new Date(2019, 3, 19) },
+  }
+]);
 </script>
 <template>
     <div class="conteiner">
+      <div class="container-esquerda">
       <div class="coluna-esquerda">
-        <div>
-          <h2 class="titulo">Sobre a loja:</h2>
-  
+        <div class="circulos">
+          <div>
           <div class="indicador">
             <div class="circulo circulo-escura">21</div>
             <span class="texto-escuro medio">Pedidos em aberto</span>
@@ -16,7 +27,8 @@ import OrcamentoProduto from '@/components/administrador/grafico/OrcamentoProdut
             <div class="circulo circulo-rosa">10</div>
             <span class="texto-rosa medio">Orçamentos de casamento</span>
           </div>
-  
+          </div>
+          <div>
           <div class="indicador">
             <div class="circulo circulo-roxa">15</div>
             <span class="texto-roxa medio">Orçamentos simples</span>
@@ -27,10 +39,6 @@ import OrcamentoProduto from '@/components/administrador/grafico/OrcamentoProdut
             <span class="texto-cinza medio">Solicitação de orçamento</span>
           </div>
         </div>
-  
-        <div class="saldo">
-          Saldo do mês: <br />
-          <span class="valor-saldo">R$ 15.000,00</span>
         </div>
       </div>
   
@@ -40,14 +48,54 @@ import OrcamentoProduto from '@/components/administrador/grafico/OrcamentoProdut
         </div>
       </div>
     </div>
+  
+  <div class="container-direita">
+    <div class="calendario">
+      <VCalendar borderless
+    :initial-page="{ month: 4, year: 2019 }"
+    :color="selectedColor"
+    :attributes="attrs"
+  />
+    </div>
+        <div class="grafico-circulo">
+          <SaboresFavoritos/>                                               
+    </div>
+   
+  </div></div>
   </template>
   
   <style scoped>
-  .conteiner {
-    display: flex;
-    justify-content: center;
+    .calendario{
+          display: flex;
+    flex-direction: column;
     align-items: center;
-    padding: 40px 100px;
+    justify-content: center;
+    width: 100%;
+    height: 30vh;
+  }
+  .grafico-circulo{
+        display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+        z-index: 9999;
+  }
+  .container-direita{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 23vw;
+  }
+  .circulos{
+    display: flex;
+    align-items: center;
+    margin:5px 0px;
+  }
+  .conteiner {
+    display:flex;
+    padding: 30px 100px;
     gap: 40px;
   }
   
@@ -55,7 +103,7 @@ import OrcamentoProduto from '@/components/administrador/grafico/OrcamentoProdut
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    width: max-content;
+    width: 60vw;
   }
   
   .titulo {
@@ -69,8 +117,8 @@ import OrcamentoProduto from '@/components/administrador/grafico/OrcamentoProdut
     display: flex;
     align-items: center;
     gap: 12px;
-    margin-bottom: 12px;
-    width: max-content;
+    margin-bottom: 22px;
+    width: 30vw;
   }
   
   .indicador-final {
@@ -131,21 +179,6 @@ import OrcamentoProduto from '@/components/administrador/grafico/OrcamentoProdut
     font-weight: 700;
   }
   
-  .saldo {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    background-color: #191645;
-    color: white;
-    font-size: 16px;
-    font-family: "Inter", sans-serif;
-    border-radius: 25px;
-    padding: 12px 16px;
-    width: 100%;
-    height: 70px;
-    font-weight: 400;
-  }
-  
   .valor-saldo {
     margin-top: 10px;
     font-weight: 500;
@@ -155,18 +188,11 @@ import OrcamentoProduto from '@/components/administrador/grafico/OrcamentoProdut
   .coluna-direita {
     display: flex;
     width: 100%;
-    justify-content: end;
   }
   
   .grafico-container {
-    display: flex;
-    height: min-content;
-    justify-content: center;
-    align-items: center;
-    width: 55vw;
-    border: 1px solid #191645;
-    border-radius: 16px;
-    padding: 16px;
+    width: 100%;
+    height: 55vh;
   }
   
   .grafico-imagem {
