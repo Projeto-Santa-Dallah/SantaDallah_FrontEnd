8322844ba0a1c0404d8551bd6bfbd48fc596ef6b
isabellirosa <isabelli.ifc@gmail.com>
2025-09-14 21:29:31 -0300

    Feat: organizando produtos view #14

--- src/views/administrador/ProdutosView.vue
+++ src/views/administrador/ProdutosView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import AdminFiltro from '@/components/filtros/AdminFiltro.vue';
 import AddProduto from '@/components/filtros/AddProduto.vue';
 import ProdutosAdm from '@/components/administrador/ProdutosAdm.vue';
@@ -15,39 +15,48 @@ function closeProduto() {
 }
 </script>
 
-<template>   
+<template>
 
     <div class="filtro-produtos">
-        <AdminFiltro><AddProduto @open="openProduto()" /></AdminFiltro>
-         <CadastrarProduto :open="openAddProduto" @close="closeProduto()" v-if="openAddProduto"/>
-        <ProdutosAdm v-else>  <div class="produtos-header">
-        <div class="header"><h1 class="titulo-produtos">Produtos</h1><span>  (58 encontrados)</span>
-  </div>  </div></ProdutosAdm>
+        <AdminFiltro>
+            <AddProduto @open="openProduto()" />
+        </AdminFiltro>
+        <CadastrarProduto :open="openAddProduto" @close="closeProduto()" v-if="openAddProduto" />
+        <ProdutosAdm v-else>
+            <div class="produtos-header">
+                <div class="header">
+                    <h1 class="titulo-produtos">Produtos</h1><span> (58 encontrados)</span>
+                </div>
+            </div>
+        </ProdutosAdm>
 
-</div>
+    </div>
 </template>
 <style scoped>
-span{
-    color:#BF99C8;
+span {
+    color: #BF99C8;
     font-size: 16px;
 }
-h1{
+
+h1 {
     margin-right: 10px;
     color: #191645;
     font-size: 24px;
 }
-.header{
+
+.header {
     display: flex;
     align-items: center;
 }
-.filtro-produtos{
+
+.filtro-produtos {
     display: flex;
     flex-direction: row;
     margin-top: 80px;
 }
+
 .titulo-produtos {
     font-size: 24px;
     font-weight: 600;
 }
-
 </style>
